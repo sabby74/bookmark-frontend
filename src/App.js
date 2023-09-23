6b4db2cb99a555e457bdfe5055619b8cdeabd7a4
Syed Ahmed <syedahmed@syeds-air.mynetworksettings.com>
2023-09-23 12:29:50 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
-import Index from "./pages/Index";
 import { Outlet } from "react-router-dom";
 
 function App() {
--- src/actions.js
+++ src/actions.js
@@ -20,3 +20,35 @@ export const createAction = async ({ request }) => {
   // redirect to index
   return redirect("/")
 }
+
+export const deleteAction = async ({params}) => {
+  await fetch(URL + '/bookmarks/' + params.id, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  return redirect("/")
+}
+
+export const updateAction = async ({ params, request }) => {
+  const formData = await request.formData()
+  console.log(updateAction);
+
+  const bookmark = {
+      title: formData.get('title'),
+      url: formData.get('url'),
+  };
+  console.log(bookmark, ': Bookmark to be updated');
+
+  await fetch(URL + '/bookmarks/' + params.id, {
+      method: "put",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookmark),
+  })
+
+  return redirect(`/${params.id}`)
+};
